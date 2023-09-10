740fbe598c3b66d77204283320066b2030a980e9
dacasfilipe <filiperdacas@gmail.com>
2023-09-10 10:02:35 -0300

    add fixes in databases

--- medicSearchAdmin/settings/settings.py
+++ medicSearchAdmin/settings/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = "medicSearchAdmin.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-}
+}'''
 
 
 # Password validation
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
