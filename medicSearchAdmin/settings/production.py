b320ea728424659cacdb019870becc10e00567aa
dacasfilipe <filiperdacas@gmail.com>
2023-09-10 09:55:30 -0300

    add file production.py

---
+++ medicSearchAdmin/settings/production.py
@@ -0,0 +1,14 @@
+import os
+
+from .settings import *
+
+DEBUG = False
+
+SECRET_KEY = 'ixb62ha#ts=ab4t2u%p1_62-!5w2j==j6d^3-j$!z(@*m+-h'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME' : os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
